4788afeb4b7264e95eed9781e92cef309cd7de93
christianCITS <christian.cioetaa@gmail.com>
2024-04-18 18:34:50 +0200

    aggiunti i primi esercizi della lezione 2

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -2,3 +2,32 @@
 #18/04/2024
 
 print("Hello World")
+
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+# Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+#this variable contain a name
+a:str="Claudio"
+print("hello" , a , "Would you like to learn...")
+
+#this variable contain a name
+b="Antonio"
+print(b.lower(),b.upper(),b.title())
+ 
+
+print ("Giuseppe Ungaretti una volta disse:"  ' "Mi illumino di immenso."')
+
+
+
+famous_person: str="Giuseppe Ungaretti"
+message:str=' "Mi illumino di immenso."'
+
+
+print(famous_person ,"una volta disse:",message)
+
+
+
+filename='python_notes.txt'
+filename.removesuffix()
+print(filename)
+
