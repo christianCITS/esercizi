e9593ec36d31d7add6fe155fe08871f4a843165d
christianCITS <christian.cioetaa@gmail.com>
2024-04-18 19:40:39 +0200

    esercizio 3-2 terminato

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -51,6 +51,17 @@ print(names[0])
 print(names[1])
 print(names[2])
 print(names[3])
+
+#3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. 
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+print(f"{names[0]} Sei stato invitato alla festa.")
+print(f"{names[1]} Sei stato invitato alla festa.")
+print(f"{names[2]} Sei stato invitato alla festa.")
+print(f"{names[3]} Sei stato invitato alla festa.")
+
+
+
+
  
 
 
