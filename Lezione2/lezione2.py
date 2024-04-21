de5c502107f9f04d1a799d4d01c7a24c92a2c403
christianCITS <christian.cioetaa@gmail.com>
2024-04-21 15:27:50 +0200

    prova git

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -61,6 +61,7 @@ print(f"{names[3]} Sei stato invitato alla festa.")
 
 #3-3. Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 #Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+#aaa
 
 
 
