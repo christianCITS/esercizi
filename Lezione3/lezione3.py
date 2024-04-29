1e39251f071c0c0a1dd70d388ae28bf0c2d44dc7
christianCITS <christian.cioetaa@gmail.com>
2024-04-30 00:34:17 +0200

    esercizio 4-12 terminato

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -128,10 +128,12 @@ for x in friend_pizzas:
 
 
 
+#4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space. 
+#Choose a version of foods.py, and write two for loops to print each list of foods.
 
-
-
-
-
+print("esercizio 4-12: ")
+for x in pizza:
+    for y in friend_pizzas:
+        print(x,y)
 
 
