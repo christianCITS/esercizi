06604683be466069170ad036c19c0d29173a2f2d
christianCITS <christian.cioetaa@gmail.com>
2024-04-29 22:21:58 +0200

    esercizio 4-6 terminato

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -68,6 +68,13 @@ print(max(one_million))
 print(sum(one_million))
 
 
+#4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+num_disp:list[int]=[i for i in range(1,21,2)]
+print(num_disp)
+
+
+
+
 
 
 
