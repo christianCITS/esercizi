1c4dc17246ace7c786786dd45addbadbd456f5a9
christianCITS <christian.cioetaa@gmail.com>
2024-04-29 22:11:53 +0200

    esercizio 4-5 terminato

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -47,3 +47,27 @@ for i in range(1,21):
 
 
 
+#4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+
+
+one_million:list[int]=[x for x in range(1,1000001)]
+
+#for i in one_million:
+    #print(i)
+
+
+
+
+
+
+
+#4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. 
+#Also, use the sum() function to see how quickly Python can add a million numbers.
+print(min(one_million))
+print(max(one_million))
+print(sum(one_million))
+
+
+
+
+
