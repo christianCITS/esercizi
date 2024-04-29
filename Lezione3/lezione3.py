a38ad5e667ecafd2688b1e7b17b88e9990f13ec2
christianCITS <christian.cioetaa@gmail.com>
2024-04-30 01:26:04 +0200

    esercizio 5-1 in corso

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -133,7 +133,38 @@ for x in friend_pizzas:
 
 print("esercizio 4-12: ")
 for x in pizza:
-    for y in friend_pizzas:
-        print(x,y)
+    print(x)
+for y in friend_pizzas:
+    print(y)
+
+
+#5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+#describing each test and your prediction for the results of each test. Your code
+#should look something like this:
+#car = 'subaru'
+#print("Is car == 'subaru'? I predict True.")
+#print(car == 'subaru')
+#print("\nIs car == 'audi'? I predict False.")
+#print(car == 'audi')
+#• Look closely at your results, and make sure you understand why each line
+#evaluates to True or False.
+#• Create at least 10 tests. Have at least 5 tests evaluate to True and another
+#5 tests evaluate to False.
+
+bike:str="bici"
+print("is bike == bici? I predict True.")
+print(bike=="bici")
+print("is bike == macchina? I predict False.")
+print(bike=="macchina")
+mcDonald:str="Fast Food"
+print("is mcDonald == Fast Food? I predict True.")
+print(mcDonald=="Fast Food")
+print("is mcDonald == Restaurant? I predict False.")
+print(mcDonald=="Restaurant")
+samsung:str="Cellulare"
+print("is samsung == Cellulare? I predict True.")
+print(samsung=="Cellulare")
+print("is samsung == Profumo? I predict False.")
+print(samsung=="Profumo")
 
 
