ddc7b4d41e6488020c896f59142c181577e859d3
christianCITS <christian.cioetaa@gmail.com>
2024-04-30 20:23:59 +0200

    esercizio 8-1 terminato

---
+++ Lezione4/lezione4.py
@@ -0,0 +1,7 @@
+#8-1. Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. 
+#Call the function, and make sure the message displays correctly.
+print("\nESERCIZIO 8-1")
+def Display_Message() ->(str):
+    print("In questo capitolo impareremo ad utilizzare le funzioni in modo corretto.")
+
+Display_Message()
