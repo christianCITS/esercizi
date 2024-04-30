983659e0772d0e96ce6cc74df07c79ab1d6a830a
christianCITS <christian.cioetaa@gmail.com>
2024-04-30 15:42:25 +0200

    esercizio 5-4 terminato

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -190,22 +190,39 @@ print(bike in "bici")
 print("\nESERCIZIO 5-3")
 alien_color:str="red"
 if alien_color is "red":
-    print("You earned 5 points!")
+    print("\nYou earned 5 points!")
 else:
     print("\n")
 
 
 
-if alien_color is "green":
+if alien_color == "green":
     print("\n")
-elif alien_color is "yellow":
+elif alien_color == "yellow":
      print("\n")
-elif alien_color is "red":
+elif alien_color == "red":
+    print("\nYou earned 5 points!")
+
+
+
+
+
+
+#5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+# If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+# If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# Write one version of this program that runs the if block and another that runs the else block.
+print("\nESERCIZIO 5-4")
+alien_color2:str="green"
+
+if alien_color2 == "green":
     print("You earned 5 points!")
-
-
-
-
+elif alien_color2 == "red":
+    print("You earned 10 points!")
+elif alien_color2 == "yellow":
+    print("You earned 10 points!")
+else:
+    print("\n")
 
 
 
