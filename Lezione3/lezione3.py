14f434ca1f0b51be2c594265bf2c0cd94f2f3874
christianCITS <christian.cioetaa@gmail.com>
2024-04-30 17:06:16 +0200

    esercizio 5-6 in corso

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -240,6 +240,20 @@ else:
     print("\n")
 
 
+#5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+# If the person is less than 2 years old, print a message that the person is a baby.
+# If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# If the person is age 65 or older, print a message that the person is an elder.
+print("\nESERCIZIO 5-6")
+age:int=int(input("Inserisci l'età desiderata: ").strip())
+if age <2:
+    print("\nThe person is a baby")
+elif 2>= age <=4:
+    print("\nThe person is a toddler")
+
 
 
 
