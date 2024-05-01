075ec85e11a0d8b4bd4e995170e185aa8c9f7624
christianCITS <christian.cioetaa@gmail.com>
2024-05-01 22:16:56 +0200

    esercizio 8-8 terminato

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -89,6 +89,19 @@ print(make_album("Artic Monkeys","AM",12))
 
 
 
+#8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
+#Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. 
+#Be sure to include a quit value in the while loop.
+print("\nESERCIZIO 8-8")
+a:str="c"
+while a != "s" :
+    a:str=input("Inserire album: ")
+    b:str=input("Inserire titolo canzone: ")
+    if a =="s":
+        break
+    print(make_album(a,b))
+
+
 
 
 
