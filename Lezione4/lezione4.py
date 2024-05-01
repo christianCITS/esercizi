691881246a9454cee899f315c5cfde73dce61a20
christianCITS <christian.cioetaa@gmail.com>
2024-05-01 19:09:58 +0200

    esercizio 8-3 terminato

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -5,3 +5,34 @@ def Display_Message() ->(str):
     print("In questo capitolo impareremo ad utilizzare le funzioni in modo corretto.")
 
 Display_Message()
+
+#8-2. Favorite Book: Write a function called favorite_book() that accepts one parameter, title. 
+#The function should print a message, such as "One of my favorite books is Alice in Wonderland". 
+#all the function, making sure to include a book title as an argument in the function call
+print("\nESERCIZIO 8-2")
+def Favorite_Book(title) ->(str):
+    print(f"\nUno dei miei libri preferiti è: {title}.")
+
+
+
+Favorite_Book("Il nome del vento")
+
+
+
+#8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+#The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+#Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.
+
+
+def Make_Shirt(size:(str),text:(str))->(str):
+    print(f"\nLa taglia della T-Shirt è una: {size}, ed il messaggio è: {text}.")
+
+
+
+Make_Shirt("L","Nella vecchia fattoria")
+Make_Shirt(size="L",text="Nella vecchia fattoria")
+
+
+
+
+
