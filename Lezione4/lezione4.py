972c8c731a3f1f28bb7b0d00e345994262793362
christianCITS <christian.cioetaa@gmail.com>
2024-05-01 21:36:57 +0200

    esercizio 8-7 terminato

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -68,6 +68,35 @@ print(city_country("Bucarest","Romania"))
 
 
 
+#8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
+#The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. 
+#Use the function to make three dictionaries representing different albums. Print each return value to show that the  dictionaries are storing the album information correctly. 
+#Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. If the calling line includes a value for the number of songs, add that value to the album’s dictionary. 
+#Make at least one new function call that includes the number of songs on an album.
+print("\nESERCIZIO 8-7")
+def make_album(artist:str,album:str,make_album:int=None) -> (dict[str,(str,int)]):
+    art_album:dict[str,(str,int)]={}
+    art_album[artist]=album,make_album
+    return art_album
+
+
+
+print(make_album("Kendrick Lamar","good kid, m.A.A.d city",))
+print(make_album("Billie Eilish","When We All Fall Asleep, Where Do We Go?",14))
+print(make_album("Artic Monkeys","AM",12))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
