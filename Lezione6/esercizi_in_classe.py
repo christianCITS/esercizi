766a5cc2e2751c0567269ceb670fbc2e2d2a165e
christianCITS <christian.cioetaa@gmail.com>
2024-05-08 13:30:56 +0200

    inizio progetto zoo.py

---
+++ Lezione6/esercizi_in_classe.py
@@ -0,0 +1,50 @@
+"""class Food:
+    def __init__(self,name:str,price:float,descr:str):
+        self.name=name
+        self.price=price
+        self.descr=descr
+
+
+
+
+
+        mozzarella=Food("Mozzarella",3,99,"Alimento prelibato proveniente dalla lavorazione del latte")
+        mela=Food("Mela",0.99,"Frutto molto nutriente.")
+        pasta=Food("pasta",0.65,"Alimento proveniente dalla lavorazione del grano")
+
+
+class Menu:
+    def __init__(self,foods:list[Food]):
+        self.foods=foods
+       
+
+    def addFood(self,food:Food):
+        self.foods.append(food)
+        
+    
+    def removeFood(self,food:Food):
+        self.foods.remove(food)"""
+
+
+
+
+
+##################################################################################################################################
+
+
+
+
+
+
+    
+        
+        
+        
+
+
+
+
+
+
+    
+
---
+++ Lezione6/zoo.py
@@ -0,0 +1,56 @@
+class Zoo:
+    def __init__(self,fences,zookeeper):
+        self.fences=fences
+        self.zookeeper=zookeeper
+
+
+    
+
+
+
+class Animal:
+    def __init__(self,name:str,species:str,age:int,height:float,wight:float,pre_habitat:str,health:float):
+        self.name=name
+        self.species=species
+        self.age=age
+        self.height=height
+        self.wight=wight
+        self.pre_habitat=pre_habitat
+        self.health=health
+
+
+
+class Fence:
+    def __init__(self,temperature:float,area:float,habitat:str):
+        self.temperature=temperature
+        self.area=area
+        self.habitat=habitat
+
+    
+
+
+class Zookeeper:
+    def __init__(self,name:str,surname:str,id:int):
+        self.name=name
+        self.surname=surname
+        self.id=id
+
+        
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
