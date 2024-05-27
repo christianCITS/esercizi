23989c0d969672342c48a571eb52e875cfd88444
christianCITS <christian.cioetaa@gmail.com>
2024-05-28 01:47:33 +0200

    finire  funzione gara() con le altre funzioni all'interno

--- Lezione7/progetto_lepre_tartaruga.py
+++ Lezione7/progetto_lepre_tartaruga.py
@@ -1,17 +1,23 @@
 import random
-def visualizza_pos(pos_t,pos_l):
-    tracciato:list[int]=['_']*70
-    t:str="T"
-    h:str="H"
-    
+t:int=4
+h:int=0
+
+def visualizza_pos(t:int,h:int):
+    tracciato:list[str]=["_"]*70
+    if t==h:
+          print("OUCH")
+    for _ in range(len(tracciato)):
+          tracciato[t]="T"
+          tracciato[h]="H"
 
 
-def tartaruga(pos,dado):
+          
+def tartaruga(pos):
     dado:int=random.randint(1,10)
     if 1<= dado<=5:
            pos+=3
     if 6<= dado <=7:
-           pos+=6
+           pos-=6
     if 8<=dado<=10:
            pos+=1
     return pos   
@@ -19,16 +25,32 @@ def tartaruga(pos,dado):
 
 
     
-def lepre(pos,dado):
+def lepre(pos):
       dado:int=random.randint(1,10)
       if 1<= dado<=2:
             pass
+      if 3<= dado <=4:
+            pos+=9
+      if dado==5:
+            pos-=12
+      if 6<= dado <=8:
+            pos +=1
+      if 9<= dado <=10:
+            pos-=2
+      return pos
+
+
+def gara():
+      while True:
+            
+            
       
+
+
       
-        
 
 
-
+   
 
 
 
