6768c5b409abaecc94ff62a2c84ab2230ee8793a
christianCITS <christian.cioetaa@gmail.com>
2024-05-30 19:23:49 +0200

    commit appunti vari

--- Lezione7/appunti_vari.py
+++ Lezione7/appunti_vari.py
@@ -1,25 +1,4 @@
-'''Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', write a function to determine if the input string is valid.
 
-An input string is valid if: 
-
-    Open brackets must be closed by the same type of brackets.
-    Open brackets must be closed in the correct order.
-    Every close bracket has a corresponding open bracket of the same type.
-
-For example:
-Test 	Result
-
-print(is_valid_parenthesis("()"))
-
-	
-
-True
-
-print(is_valid_parenthesis("(]"))
-
-	
-
-False'''
 
 
 
