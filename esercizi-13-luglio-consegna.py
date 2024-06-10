e4d3f71eec8151abe46274b18eea8e01ec7848ca
christianCITS <christian.cioetaa@gmail.com>
2024-06-10 19:32:11 +0200

    finire ultimo esercizio consegna 13 luglio

--- esercizi-13-luglio-consegna.py
+++ esercizi-13-luglio-consegna.py
@@ -72,38 +72,207 @@ Classe:
 
 class RecipeManager:
     def __init__(self):
-        self.lista_diz:list[dict[str,list[str]]]=[]
+        self.diz:dict[str,list[str]]={}
 
 
     def create_recipe(self,recipe_name:str, ingredient:list[str])-> dict[str,list[str]]:
-        diz_ricetta:dict[str,list[str]]={}
-        diz_ricetta[recipe_name]=ingredient
-        if diz_ricetta in self.lista_diz:
-            print(f"La ricetta esiste già")
-        self.lista_diz.append(diz_ricetta)
-        return diz_ricetta
+        for k in  self.diz.keys():
+            if k== recipe_name:
+                return f"La ricetta esiste già"
+        self.diz[recipe_name]=ingredient
+        return self.diz
     
 
 
+    def add_ingredient(self,recipe_name, ingredient):
+        for k,v in self.diz.items():
+            if k== recipe_name and ingredient not in v:
+                v.append(ingredient)
+        return self.diz
+
 
     def remove_ingredient(self,recipe_name:str, ingredient:str):
-        for r in self.lista_diz:
-            if r==recipe_name and self.diz_ricetta[recipe_name]== ingredient:
+        for k,v in self.diz.items():
+            if k== recipe_name and ingredient in v :
+                v.remove(ingredient)
+            else:
+                print("l'ingrediente non è nella ricetta")   
+        return self.diz
+
+
+    def update_ingredient(self,recipe_name:str, old_ingredient:str, new_ingredient:str):
+        for k,v in self.diz.items():
+            if k== recipe_name and old_ingredient in v:
+                if new_ingredient not in v:
+                    for i,c in enumerate(v):
+                        if c == old_ingredient:
+                            v[i]=new_ingredient          
+        return self.diz
+    
+
+
+    def list_recipes(self):
+        lista:list=[]
+        for k in self.diz.keys():
+            lista.append(k)
+        return lista
+
+
+    def list_ingredients(self,recipe_name):
+        for k,v in self.diz.items():
+            if k== recipe_name:
+                return v
+            else:
+                print("La ricetta non esiste")
+
+
+
+    def search_recipe_by_ingredient(self,ingredient):
+        for k,v in self.diz.items():
+            if ingredient in v:
+                return self.diz
+
                 
                 
-                
 
 
 
 
 
-recipe=RecipeManager()
+'''In questo esercizio, creeremo una gerarchia di classi per rappresentare diversi tipi di veicoli.
+1. Classe Base: Veicolo
+Crea una classe base chiamata Veicolo con i seguenti attributi e metodi:
+ 
+Attributi:
 
+    marca (stringa)
+    modello (stringa)
+    anno (intero)
 
-recipe.create_recipe("pasta",["acqua","farina","uova","sale"])
-recipe.remove_ingredient("pasta","acqua")
+Metodi:
+
+    __init__(self, marca, modello, anno): metodo costruttore che inizializza gli attributi marca, modello e anno.
+    descrivi_veicolo(self): metodo che stampa una descrizione del veicolo nel formato "Marca: [marca], 
+    Modello: [modello], Anno: [anno]".
+
+2. Classe Derivata: Auto
+Crea una classe derivata chiamata Auto che eredita dalla classe Veicolo e aggiunge i seguenti attributi e metodi:
+ 
+Attributi:
+
+    numero_porte (intero)
+
+Metodi:
+
+    __init__(self, marca, modello, anno, numero_porte): metodo costruttore che inizializza gli attributi della classe base e numero_porte.
+   descrivi_veicolo(self): metodo che sovrascrive quello della classe base per includere anche il numero di porte nella descrizione, nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Numero di porte: [numero_porte]".
+
+3. Classe Derivata: Moto
+Crea una classe derivata chiamata Moto che eredita dalla classe Veicolo e aggiunge i seguenti attributi e metodi:
+ 
+Attributi:
+
+    tipo (stringa, ad esempio "sportiva", "cruiser", ecc.)
+
+Metodi:
+
+    __init__(self, marca, modello, anno, tipo): metodo costruttore che inizializza gli attributi della classe base e tipo.
+    descrivi_veicolo(self): metodo che sovrascrive quello della classe base per includere anche il tipo di moto nella descrizione, nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Tipo: [tipo]".'''
+
 
         
+
+
+class Veicolo:
+    def __init__(self,marca:str,modello:str,anno:int):
+        self.marca:str=marca
+        self.modello:str=modello
+        self.anno:int=anno
+
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}")
+
+
+
+class Auto(Veicolo):
+
+    def __init__(self, marca: str, modello: str, anno: int,numero_porte:int):
+        super().__init__(marca, modello, anno)
+        self.numero_porte=numero_porte
+
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Numero di porte: {self.numero_porte}")
+
+
+class Moto(Veicolo):
+    def __init__(self, marca: str, modello: str, anno: int,tipo:str):
+        super().__init__(marca, modello, anno)
+        self.tipo:str=tipo
+
+
+    def descrivi_veicolo(self):
+                print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Tipo: {self.tipo}")
+
+
+
+
+
+
+
+
+
+
+
+
+'''Obiettivo
+L'obiettivo di questo esercizio è creare un modello semplice per simulare la crescita delle popolazioni di due specie animali usando la programmazione orientata agli oggetti in Python.
+
+Descrizione del problema
+Due specie animali, i Bufali Klingon e gli Elefanti, vivono in una riserva naturale. Ogni specie ha una popolazione iniziale e un tasso di crescita annuo. Vogliamo sapere:
+- In quanti anni la popolazione degli Elefanti supererà quella dei Bufali Klingon.
+- n quanti anni la popolazione dei Bufali Klingon raggiungerà una densità di 1 individuo per km².
+ 
+Specifiche tecniche
+
+1. Classe Specie
+- Attributi:
+
+    nome (str): Nome della specie.
+    popolazione (int): Popolazione iniziale.
+    tasso_crescita (float): Tasso di crescita annuo percentuale.
+
+- Metodi:
+
+    __init__(self, nome: str, popolazione_iniziale: int, tasso_crescita: float): Costruttore per inizializzare gli attributi della classe.
+    cresci(self): Metodo per aggiornare la popolazione per l'anno successivo.
+    anni_per_superare(self, altra_specie: 'Specie') -> int: Metodo per calcolare in quanti anni la popolazione di questa specie supererà quella di un'altra specie.
+    getDensita(self, area_kmq: float) -> int: Metodo per calcolare in quanti anni la popolazione raggiungerà una densità di 1 individuo per km².
+
+ 
+
+2. Sottoclassi BufaloKlingon e Elefante
+Entrambe le sottoclassi animali BufaloKlingon ed Elefante devono ereditare dalla classe base Specie e devono inizializzare il nome della specie rispettiva.
+ 
+Formule Matematiche:
+
+    Aggiornamento della popolazione per l'anno successivo:
+        Formula: popolazione_nuova = popolazione_attuale x (1 + tasso_crescita/100)
+    Calcolo della densità di popolazione:
+        Formula: popolazione / area_kmq
+        Hint: Loop incrementale che continua ad aggiornare la popolazione finché la densità non raggiunge 1 individuo per km²
+    Calcolo degli anni necessari per superare la popolazione di un'altra specie:
+        Hint: Loop incrementale che continua ad aggiornare la popolazione di entrambe le specie finché la popolazione di questa specie non supera quella dell'altra. Per evitare che le popolazioni crescano all'infinito, limitare il numero di anni a 1000. '''
+
+
+
+        
+    
+class Specie:
+    def __init__(self,nome:str,popolazione:int,tasso_crescita):
+
+        
+
+
 
 
 
