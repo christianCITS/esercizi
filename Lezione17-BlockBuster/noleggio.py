8986aa36e116bcbfddb866be3a46a21be3dd33a3
christianCITS <christian.cioetaa@gmail.com>
2024-06-21 13:49:42 +0200

    finire impiegati studenti ed ultime due funzioni di block buster

--- Lezione17-BlockBuster/noleggio.py
+++ Lezione17-BlockBuster/noleggio.py
@@ -1,4 +1,5 @@
 from film import Film
+from movie_genre import Azione, Commedia,Drama
 
 class Noleggio:
     def __init__(self,film_list:list[Film]):
@@ -20,21 +21,50 @@ class Noleggio:
 
 
     def rentAmovie(self,film:Film,clientID:int):
-        film_noleggiati:list[Film]=[]
         if film not in self.film_list:
             print(f"Il film non è disponibile per il noleggio!: {film.getTitle()}")
+        if clientID not in self.rented_film:
+           film_noleggiati:list[Film]=[]
+           self.film_list.remove(film)
+           film_noleggiati.append(film)
+           self.rented_film[clientID]=film_noleggiati
+           print(f"il cliente {clientID} ha potuto noleggiare il film: {film.getTitle()}")
         else:
-            self.film_list.remove(film)
-            film_noleggiati.append(film)
-            self.rented_film[clientID]=film_noleggiati
-            print(f"il cliente {clientID} ha potuto noleggiare il film: {film.getTitle()}")
+            self.rented_film[clientID].append(film)
+            print(f"il cliente {clientID} ha noleggiato un altro  film: {film.getTitle()}")
+
+
+
+    
+    def giveBack(self,film:Film, clientID, days):
+        for fil in self.rented_film.items():
+            
+
+ 
+ 
+ 
+ 
+    #questo metodo consente di restituire un film noleggiato in negozio, ed ha come argomenti il film da restituire, 
+    # il codice ID del client che restituisce il film, il numero dei giorni in cui il cliente ha tenuto il film per se.  
+    # Il film da restituire deve essere rimosso dalla lista dei film noleggiati dal cliente con id clientID, e tale film, 
+    # deve essere riaggiunto alla lista dei film disponibili in negozio (film_list). Successivamente, 
+    # il metodo deve calcolare la penale che il cliente deve pagare utilizzando l'opposito metodo. 
+    # Stampare la penale che il cliente deve pagare: "Cliente: {clientID}! La penale da pagare per il film {titolo_film} 
+    # e' di {tot} euro!".
+
+
+
+
+
+
+
                 
 
 
-film1 = Film(112,"Inception")
-film2 = Film(111,"Interstellar")
+film1 =Azione(112,"Inception")
+film2 = Azione(111,"Interstellar")
 noleggio = Noleggio([film1,film2])
-#print(noleggio.film_list)
+print(noleggio.film_list)
 
 
 noleggio.rentAmovie(film1,112)
@@ -42,7 +72,9 @@ noleggio.rentAmovie(film2,112)
 
 
 
+print(noleggio.rented_film)
 
+noleggio.giveBack(film1,112,3)
 
 
                 
