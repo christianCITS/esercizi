f4229caa1e6c9abf8ba625df81b4a8d449ad9e73
christianCITS <christian.cioetaa@gmail.com>
2024-07-16 13:41:57 +0200

    commi java

---
+++ web1/JavaScript/esercizio1.js
@@ -0,0 +1,34 @@
+// const parseArguments = require("body/parse-arguments");
+
+// var A;
+// var B;
+// A = 3;
+// B = "buongiorno a tutti";
+// console.log("Il tipo della var A e' " + typeof(A) + "\n");
+
+
+
+// var iPosizione;
+// var sMIaStringa;
+// sMiaStringa="buonasera"
+// iPosizione ==sMIaStringa.indexOf("f")
+// if(iPosizione < 0):
+
+
+
+
+var sStringa;  
+var sTermine;
+sStringa="paperino"
+sTermine=sStringa.indexOf("ino")
+if ((sTermine=0))  (sTermine >4)
+                    
+
+
+
+
+
+
+
+
+                
