0c5993a6447e8dd8bd3b3a4a3551475d43ac7eec
christianCITS <christian.cioetaa@gmail.com>
2024-09-06 18:44:49 +0200

    commit myApp e dockerfile

--- Lezione27Python5/myApp.py
+++ Lezione27Python5/myApp.py
@@ -1,8 +1,13 @@
 import time 
 import sys
-while True:
-    print("prova print")
+sys.stdout=open("./file_per_le_print.txt","w+")
+ii=1
+while (ii<10):
     sys.stdout.flush()
     time.sleep(2)
-    
+    print("prova print")
+    ii=1
+sys.stdout.close()
+
+
 
