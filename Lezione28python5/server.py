69ed033d45aa2f0117d23d7dc38cab055e296c2f
christianCITS <christian.cioetaa@gmail.com>
2024-09-10 11:36:38 +0200

    commit server.py e creazione dockerfile

---
+++ Lezione28python5/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+api=Flask("__name__")
+@api.route('/provanumero1', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@api.route('/provanumero2', methods=['GET'])
+def index2():
+    return render_template('index2.html')
+
+
+
+
+api.run(host="0.0.0.0", port=8085)
+
