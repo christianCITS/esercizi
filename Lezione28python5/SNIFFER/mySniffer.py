3f99339d4f83de3af38fc0605aaebcce37e6f159
christianCITS <christian.cioetaa@gmail.com>
2024-09-13 13:53:03 +0200

    commit continuo mySniffer

--- Lezione28python5/SNIFFER/mySniffer.py
+++ Lezione28python5/SNIFFER/mySniffer.py
@@ -1,17 +1,55 @@
 from scapy.all import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP,TCP
 from scapy.layers.http import HTTPRequest 
 from scapy.layers.http import HTTPResponse
+from datetime import datetime
+import csv
 
 
 
-
+iPkt=0
 
 def process_pkt(pkt):
     global iPkt
-    print(f"Ho ricevutto un package  {iPkt} lungo {pkt[IP].len}")
+    iPkt+=1
+    if pkt[TCP].dport in [80,443] or pkt[TCP].sport in [80,443]:
+        dataOra=datetime.now().strftime("%Y-%m-%d %H: %M:%S")
+        ip_src=pkt[IP].src
+        ip_dst=pkt[IP].dst
+        tcp_src=pkt[TCP].sport
+        tcp_dst=pkt[TCP].dport
 
 
-sniff(iface="en0", filter="tcp", prn=process_pkt)
+        host= ""
+        if HTTPRequest in pkt:
+            if pkt[HTTPRequest].Host:
+             host = pkt[HTTPRequest].Host.decode()
+                
+        
+        if 443 in [tcp_src,tcp_dst]:
+            protocol= "HTTPS"
+        else:
+           protocol= "HTTP"
+
+
+        with open("file.csv","a", newline="") as file:
+           writer=csv.writer(file)
+           writer.writerow([f"{dataOra}|,{ip_src}|,{ip_dst}|,{tcp_src}|,{tcp_dst}"])
+
+
+
+
+with open("file.csv","w", newline="") as file:
+    writer=csv.writer(file)
+    writer.writerow(["dataOra|","ip_src|","ip_dst|","tcp_src|","tcp_dst|"])
+
+           
+           
+
+
+sniff(iface="eth0", filter="tcp", prn=process_pkt)
+
+
+
 
 
