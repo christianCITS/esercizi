6f96c867cf0590c15c4251e18e45db50aff5eafb
christianCITS <christian.cioetaa@gmail.com>
2024-09-19 19:18:13 +0200

    esercitazione flask e registrazione utenti da terminare

--- Lezione28python5/SNIFFER/mySniffer.py
+++ Lezione28python5/SNIFFER/mySniffer.py
@@ -12,16 +12,16 @@ iPkt=0
 def process_pkt(pkt):
     global iPkt
     iPkt+=1
-    if pkt[TCP].dport in [80,443] or pkt[TCP].sport in [80,443]:
+    if pkt.haslayer(TCP) and (pkt[TCP].dport in [80, 443] or pkt[TCP].sport in [80, 443]):        
         dataOra=datetime.now().strftime("%Y-%m-%d %H: %M:%S")
         ip_src=pkt[IP].src
         ip_dst=pkt[IP].dst
         tcp_src=pkt[TCP].sport
         tcp_dst=pkt[TCP].dport
-
-
         host= ""
-        if HTTPRequest in pkt:
+
+        
+        if pkt.haslayer(HTTPRequest):
             if pkt[HTTPRequest].Host:
              host = pkt[HTTPRequest].Host.decode()
                 
@@ -34,14 +34,14 @@ def process_pkt(pkt):
 
         with open("file.csv","a", newline="") as file:
            writer=csv.writer(file)
-           writer.writerow([f"{dataOra}|,{ip_src}|,{ip_dst}|,{tcp_src}|,{tcp_dst}"])
+           writer.writerow([f"{dataOra}|,{ip_src}|,{ip_dst}|,{tcp_src}|,{tcp_dst},{host}"])
 
 
 
 
 with open("file.csv","w", newline="") as file:
     writer=csv.writer(file)
-    writer.writerow(["dataOra|","ip_src|","ip_dst|","tcp_src|","tcp_dst|"])
+    writer.writerow(["dataOra|","ip_src|","ip_dst|","tcp_src|","tcp_dst|","host"])
 
            
            
--- Lezione28python5/server.py
+++ Lezione28python5/server.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+utenti = [
+          ["Patricio","NAMO123","0"],
+          ["Giovannino","Picante19","0"],
+          ["Giordanina","PasswordSicura","0"],
+          ["Marietta","MoLoCorco","0"]
+          ]
+
+
 
 
 api=Flask("__name__")
-@api.route('/provanumero1', methods=['GET'])
+
+@api.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@api.route('/provanumero2', methods=['GET'])
+@api.route('/registrati', methods=['GET'])
 def index2():
-    return render_template('index2.html')
+    nome= request.args.get('fname')
+    cognome= request.args.get('lname')
+    password=request.args.get('password')
+    for i in utenti:
+        if nome == i[0] and password in i:
+            return render_template('index2.html')
+
+    return render_template('indexko.html')
+
+
+
+
 
 
 
