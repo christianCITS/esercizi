f24e91074aee5b187429891aec935e21f24dc0a0
christianCITS <christian.cioetaa@gmail.com>
2024-10-10 19:10:29 +0200

    commit esercizio dimitri

--- DBpostgress dimitri/provadb.py
+++ DBpostgress dimitri/provadb.py
@@ -13,7 +13,12 @@ if cur is None:
 	print("Errore connessione al DB")
 	sys.exit()
 
-sQuery = "select * from cittadini limit 5;"
+
+
+sQuery= "insert into cittadini (codFisc, nome, cognome, dataNascita) values ('KSSMRA85M01H501Z', 'Mario', 'Rossi', '1985-01-15')"
+db.write_in_db(cur,sQuery)
+
+sQuery = "select * from cittadini;"
 iNumRows = db.read_in_db(cur,sQuery)
 for ii in range(0,iNumRows):
 	myrow = db.read_next_row(cur)
